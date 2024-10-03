23a5be933d8bf484365670c7ca081ecacf817d4f
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-10-03 16:40:54 +0300

    buildd2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@mui/icons-material']
+  },
+  ssr: {
+    noExternal: ['@mui/icons-material']
+  }
 })
+
+
