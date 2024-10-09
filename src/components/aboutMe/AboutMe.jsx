54f0149ca5aeab1093352f64238592038eba709a
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-10-10 00:51:58 +0300

    last edit9a

--- src/components/aboutMe/AboutMe.jsx
+++ src/components/aboutMe/AboutMe.jsx
@@ -19,8 +19,53 @@ const AboutMe = () => {
           <p>My main stack currently is React/Next.js in combination with SCSS and Material UI.</p>
         </div>
         <div className="shapeCircle">
+        </div>
       </div>
-      </div>
+      <div className="line1a"></div>
+      <div className="line2a"></div>
+      <div className="line3a"></div>
+      <div className="line4a"></div>
+      <div className="line5a"></div>
+      <div className="line6a"></div>
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+      <div className="line4"></div>
+      <div className="line5"></div>
+      <div className="line6"></div>
+      <div className="line7"></div>
+      <div className="line8"></div>
+      <div className="line9"></div>
+      <div className="line10"></div>
+      <div className="line11"></div>
+      <div className="line12"></div>
+      <div className="line13"></div>
+      <div className="line14"></div>
+      <div className="line15"></div>
+      <div className="line16"></div>
+      <div className="line17"></div>
+      <div className="line18"></div>
+      <div className="line19"></div>
+      <div className="line20"></div>
+      <div className="line21"></div>
+      <div className="line22"></div>
+      <div className="line23"></div>
+      <div className="line24"></div>
+      <div className="line25"></div>
+      <div className="line26"></div>
+      <div className="line27"></div>
+      <div className="line28"></div>
+      <div className="line29"></div>
+      <div className="line30"></div>
+      <div className="line31"></div>
+      <div className="line32"></div>
+      <div className="line33"></div>
+      <div className="line34"></div>
+      <div className="line35"></div>
+      <div className="line36"></div>
+      <div className="line37"></div>
+      <div className="line38"></div>
+      <div className="line39"></div>
     </div>
   )
 }
