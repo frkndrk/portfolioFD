c7f32b477fee171f46e667a92a4098a982b1db08
FURKAN DURAK <furkandurak@FURKAN-MacBook-Air.local>
2024-10-11 15:56:05 +0300

    finished

--- src/components/projects/PopUpProject.jsx
+++ src/components/projects/PopUpProject.jsx
@@ -27,7 +27,8 @@ const PopUpProject = ({ setPopUpState, popUpId }) => {
             img: service1,
             link: "https://yamantarimaletleri.com/",
             icon: tractor,
-            code: "https://github.com/frkndrk/yamantarim"
+            code: "https://github.com/frkndrk/yamantarim",
+            desc: "A website that promotes agricultural equipment. This website consists of information about the company, equipment categories, and pages with details of these equipment."
         },
         {
             id: 2,
@@ -35,7 +36,8 @@ const PopUpProject = ({ setPopUpState, popUpId }) => {
             img: service2,
             link: "https://enkaymm.com/",
             icon: tax,
-            code: "https://github.com/frkndrk/certified_public_accountant_-cpa-"
+            code: "https://github.com/frkndrk/certified_public_accountant_-cpa-",
+            desc: "A site about certified public accountants. This website consists of information about this certified public accountant, categories of services provided and detailed explanations of these services."
         },
         {
             id: 3,
@@ -43,7 +45,8 @@ const PopUpProject = ({ setPopUpState, popUpId }) => {
             img: service3,
             link: "https://generaldokum.vercel.app/",
             icon: casting,
-            code: "https://github.com/frkndrk/generaldokum"
+            code: "https://github.com/frkndrk/generaldokum",
+            desc: "The homepage of the website I prepared about the die casting industry. It includes general information about the company, the products they cast and the parts of the world they export to."
         },
         {
             id: 4,
@@ -51,7 +54,8 @@ const PopUpProject = ({ setPopUpState, popUpId }) => {
             img: service4,
             video: billCreation,
             icon: bill,
-            code: "https://github.com/frkndrk/billing-system"
+            code: "https://github.com/frkndrk/billing-system",
+            desc: "Bill creation page. The desired amount of products and services can be added or removed from the cart and a discount can be applied to the cart. By saving, a bill can be created for the previously selected customer."
         },
         {
             id: 5,
@@ -59,28 +63,29 @@ const PopUpProject = ({ setPopUpState, popUpId }) => {
             img: service5,
             video: customerPage,
             icon: beauty,
-            code: "https://github.com/frkndrk/billing-system"
+            code: "https://github.com/frkndrk/billing-system",
+            desc: "An admin panel where you can access the customer's appointments, invoices, current account, money points, notes, feedback, uploaded documents and photos from the customer list, and where you can add, remove and make changes."
         }
     ]
 
     return (
-        <div className='popUpProject'  onClick={() => setPopUpState(false)}>
+        <div className='popUpProject' onClick={() => setPopUpState(false)}>
             <div className="popUp" onClick={(e) => e.stopPropagation()}>
-                <CancelIcon className="cancelIcon" onClick={() => setPopUpState(false)}/>
+                <CancelIcon className="cancelIcon" onClick={() => setPopUpState(false)} />
                 <div className="cont1" >
                     <div className="cont1A">
                         {services[popUpId - 1].video ? <video src={services[popUpId - 1].video} autoPlay controls></video> : <img src={services[popUpId - 1].img} alt="" />}
                     </div>
                     <div className="cont1B">
                         <h3>{services[popUpId - 1].name} <img src={services[popUpId - 1].icon && services[popUpId - 1].icon} alt="" /></h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <p>{services[popUpId - 1].desc}</p>
                         <div className="language">
                             <h4>React</h4>
                             <h4>SCSS</h4>
                         </div>
                         <div className="source">
                             <h5>Code <a href={services[popUpId - 1].code} target="_blank"><GitHubIcon /></a></h5>
-                            {!services[popUpId - 1].video && <h5>Live {services[popUpId - 1].id === 3 && "Demo"} <a href={services[popUpId - 1].link} target="_blank" style={{color: "#000", marginLeft: "5px", marginTop: "3px"}}><AdsClickIcon /></a></h5>}
+                            {!services[popUpId - 1].video && <h5>Live {services[popUpId - 1].id === 3 && "Demo"} <a href={services[popUpId - 1].link} target="_blank" style={{ color: "#000", marginLeft: "5px", marginTop: "3px" }}><AdsClickIcon /></a></h5>}
                         </div>
                     </div>
                 </div>
